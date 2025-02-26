a22f7819cd8c13445b42475115b88805a2a4e2f6
Sahil Chourasiya <sahil.chourasiya160@gmail.com>
2025-02-26 12:02:14 -0500

    Revert "added addition comment to Shipping_Cost_Calculator.py"
    This reverts commit f07c312c255abba0db73c0c3c581ef35b00dd5ae.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,7 +1,6 @@
 # Shipping Cost Calculator
 
 # Here is a new update by sahilch-dev
-# Here is another update by sahilch-dev
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
